2e84a9ced8f0acd69a492cf6c20a1203c200c8d3
Pierre Depaz <pierre.depaz@gmail.com>
2020-01-27 11:11:14 +0100

    removed `screen` argument from create_file_reader

--- Adafruit_Video_Looper/usb_drive.py
+++ Adafruit_Video_Looper/usb_drive.py
@@ -41,6 +41,6 @@ class USBDriveReader:
         return 'Insert USB drive with compatible movies.'
 
 
-def create_file_reader(config, screen):
+def create_file_reader(config):
     """Create new file reader based on mounting USB drives."""
     return USBDriveReader(config)
